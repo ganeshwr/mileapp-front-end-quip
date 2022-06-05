4b1893147078de6ee760be522bd7731e656294e0
Ganesha Wahyudi Rawlins <ganeshwr7@gmail.com>
2022-06-05 14:41:08 +0700

    Change marker icon and app title

--- src/routes/Location/location.component.jsx
+++ src/routes/Location/location.component.jsx
@@ -75,6 +75,7 @@ const Location = () => {
       zoom={zoom}
       onLoad={onLoad}
       onUnmount={onUnmount}
+      options={{ mapTypeControl: false }}
     >
       <>
         {markers.map((marker, idx) => {
@@ -82,6 +83,14 @@ const Location = () => {
             <Marker
               key={idx}
               position={marker.pos}
+              icon={{
+                path: "M19 7c0-1.1-.9-2-2-2h-3v2h3v2.65L13.52 14H10V9H6c-2.21 0-4 1.79-4 4v3h2c0 1.66 1.34 3 3 3s3-1.34 3-3h4.48L19 10.35V7zM7 17c-.55 0-1-.45-1-1h2c0 .55-.45 1-1 1z M5 6h5v2H5zm14 7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zm0 4c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z",
+                fillColor: "#1e90ff",
+                fillOpacity: 1,
+                scale: 2,
+                strokeColor: "#3742fa",
+                strokeWeight: 2,
+              }}
               onClick={(e) => markerClickHandler(e, marker)}
             />
           );
