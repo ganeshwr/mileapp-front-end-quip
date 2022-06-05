f18c35028620b154fc04f6de7edcacbdbb633d73
Ganesha Wahyudi Rawlins <ganeshwr7@gmail.com>
2022-06-05 14:53:24 +0700

    Styling map

--- src/routes/Location/location.component.jsx
+++ src/routes/Location/location.component.jsx
@@ -5,11 +5,12 @@ import {
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-import MopedIcon from "@mui/icons-material/Moped";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const containerStyle = {
   width: "100%",
-  height: "100vh",
+  height: "100%",
 };
 // -6.900696,
 const centerDefault = {
@@ -69,45 +70,55 @@ const Location = () => {
   };
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={zoom}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-      options={{ mapTypeControl: false }}
+    <Box
+      sx={{
+        height: "700px",
+        margin: "0 50px",
+      }}
     >
-      <>
-        {markers.map((marker, idx) => {
-          return (
-            <Marker
-              key={idx}
-              position={marker.pos}
-              icon={{
-                path: "M19 7c0-1.1-.9-2-2-2h-3v2h3v2.65L13.52 14H10V9H6c-2.21 0-4 1.79-4 4v3h2c0 1.66 1.34 3 3 3s3-1.34 3-3h4.48L19 10.35V7zM7 17c-.55 0-1-.45-1-1h2c0 .55-.45 1-1 1z M5 6h5v2H5zm14 7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zm0 4c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z",
-                fillColor: "#1e90ff",
-                fillOpacity: 1,
-                scale: 2,
-                strokeColor: "#3742fa",
-                strokeWeight: 2,
-              }}
-              onClick={(e) => markerClickHandler(e, marker)}
-            />
-          );
-        })}
-        {infoOpen && selectedPlace && (
-          <InfoWindow
-            position={markers[selectedPlace.id].pos}
-            onCloseClick={() => setInfoOpen(false)}
-          >
-            <div style={divStyle}>
-              <h3>Location {+selectedPlace.id + 1}</h3>
-              <div>Drop and Delivery point {+selectedPlace.id + 1}</div>
-            </div>
-          </InfoWindow>
-        )}
-      </>
-    </GoogleMap>
+      <Typography textAlign={"center"} sx={{fontSize:"2em", marginTop:4, marginBottom: 4}}>
+        Nearest Drop-Point and Delivery-Point
+      </Typography>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={zoom}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={{ mapTypeControl: false }}
+      >
+        <>
+          {markers.map((marker, idx) => {
+            return (
+              <Marker
+                key={idx}
+                position={marker.pos}
+                icon={{
+                  path: "M19 7c0-1.1-.9-2-2-2h-3v2h3v2.65L13.52 14H10V9H6c-2.21 0-4 1.79-4 4v3h2c0 1.66 1.34 3 3 3s3-1.34 3-3h4.48L19 10.35V7zM7 17c-.55 0-1-.45-1-1h2c0 .55-.45 1-1 1z M5 6h5v2H5zm14 7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zm0 4c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z",
+                  fillColor: "#1e90ff",
+                  fillOpacity: 1,
+                  scale: 2,
+                  strokeColor: "#3742fa",
+                  strokeWeight: 2,
+                }}
+                onClick={(e) => markerClickHandler(e, marker)}
+              />
+            );
+          })}
+          {infoOpen && selectedPlace && (
+            <InfoWindow
+              position={markers[selectedPlace.id].pos}
+              onCloseClick={() => setInfoOpen(false)}
+            >
+              <div style={divStyle}>
+                <h3>Location {+selectedPlace.id + 1}</h3>
+                <div>Drop and Delivery point {+selectedPlace.id + 1}</div>
+              </div>
+            </InfoWindow>
+          )}
+        </>
+      </GoogleMap>
+    </Box>
   ) : (
     <></>
   );
